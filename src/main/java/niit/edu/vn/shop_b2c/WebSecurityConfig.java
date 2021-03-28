97006aaa1f76da19429b964f41d4425bbfb810a8
root <luann4099@gmail.com>
2021-03-28 18:37:39 +0700

    cấu hình bỏ mặc định redirect vào login

---
+++ src/main/java/niit/edu/vn/shop_b2c/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package niit.edu.vn.shop_b2c;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/*").permitAll();
+    }
+}
