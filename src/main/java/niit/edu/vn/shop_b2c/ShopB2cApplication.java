67b95b7e17236e5b12f50af91972428e380975c3
root <luann4099@gmail.com>
2021-03-28 18:19:44 +0700

    init

---
+++ src/main/java/niit/edu/vn/shop_b2c/ShopB2cApplication.java
@@ -0,0 +1,13 @@
+package niit.edu.vn.shop_b2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopB2cApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopB2cApplication.class, args);
+    }
+
+}
---
+++ src/test/java/niit/edu/vn/shop_b2c/ShopB2cApplicationTests.java
@@ -0,0 +1,13 @@
+package niit.edu.vn.shop_b2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopB2cApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
