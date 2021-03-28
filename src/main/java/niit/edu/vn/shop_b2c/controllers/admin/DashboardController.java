597761d83e86bcab6e963890c18ee63f266a2fa1
root <luann4099@gmail.com>
2021-03-28 18:54:13 +0700

    thêm layout admin lte

---
+++ src/main/java/niit/edu/vn/shop_b2c/controllers/admin/DashboardController.java
@@ -0,0 +1,15 @@
+package niit.edu.vn.shop_b2c.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class DashboardController {
+
+    @GetMapping("/layout")
+     public String getLayout(){
+         return "admin/layout";
+     }
+}
